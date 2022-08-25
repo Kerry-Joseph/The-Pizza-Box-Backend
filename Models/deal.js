cf3953cee2e2e208850adc53fcdff5261f729161
robobait <kerrykj8@yahoo.com>
2022-08-25 10:04:09 -0700

    added img defaults

--- Models/deal.js
+++ Models/deal.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose")
 const DealSchema = new mongoose.Schema({
     name: String,
     content: String,
+    img: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1611599538163-c792e7f4db19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
     price: Number
 })
 
--- Models/menuItem.js
+++ Models/menuItem.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose")
 const MenuItemSchema = new mongoose.Schema({
     category: String,
     name: String,
+    img: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1605478371310-a9f1e96b4ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
     price: Number
 })
 
